a1e23ce84933f88bc76095f8bdc30c87dff87d48
Zack M. Davis <code@zackmdavis.net>
2013-11-03 13:35:11 -0800

    initial commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+gem 'pg'
+gem 'jquery-rails'
+gem 'bcrypt-ruby', '~> 3.0.0'
+gem 'jbuilder'
+
+group :development do
+  gem 'debugger'
+  gem 'better_errors'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Superscription::Application.config.secret_token = 'f942f5333521629972b1c28ddf43ae45e36fcaa296c1215bf18564feb8b6fcae4347b41bfe899d3450137ee191e115468d7a0c4d23871a7ef1e8d0be8e4f25c0'
