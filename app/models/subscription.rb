a53cd40a919649fe56c9d0d78039f119f0743b3d
Zack M. Davis <code@zackmdavis.net>
2013-11-05 14:22:54 -0800

    separation of concerns in subscription model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -9,19 +9,39 @@ class Subscription < ActiveRecord::Base
   has_many :subscribers, :through => :user_subscriptions, :source => :user
   has_many :entries
 
-  def load_entries!
+  def parsed_feed
+    @parsed_feed ||= fetch_feed
+  end
+
+  def fetch_feed
     raw_feed = open(self.url)
     puts "Fetching feed ..."
     parsed_feed = Nokogiri::XML(raw_feed)
-    puts "Got feed!"
-    self.title = parsed_feed.xpath('//title').first.text
-    declared_period = parsed_feed.xpath('//sy:updatePeriod').text
-    declared_frequency = parsed_feed.xpath('//sy:updateFrequency').text.to_i
-    self.update_frequency = update_frequency_in_seconds(declared_period, declared_frequency)
-    self.last_build_date = parsed_feed.xpath('//lastBuildDate').text.to_datetime
+    puts "Got and parsed feed!"
+    parsed_feed
+  end
+
+  def load_everything!
+    load_info!
+    load_entries!
+  end
+
+  def load_info!
+    feed = self.parsed_feed
+    self.title = feed.xpath('//title').first.text
+    update_period_unit = feed.xpath('//sy:updatePeriod').text
+    update_period = feed.xpath('//sy:updateFrequency').text.to_i
+    self.update_frequency = update_period_in_seconds(update_period_unit, update_period)
+    self.last_build_date = feed.xpath('//lastBuildDate').text.to_datetime
     self.save
+  end
+
+  def load_entries!
+    # TODO: only load new entries rather than wastefully reloading all of them
+    self.entries.delete_all
+    feed = self.parsed_feed
     entries = []
-    parsed_feed.xpath('//item').each do |item|
+    feed.xpath('//item').each do |item|
       item_attributes = {:title => item.xpath('title').text,
        :author => item.xpath('dc:creator').text,
        :url => item.xpath('link').text,
@@ -40,10 +60,10 @@ class Subscription < ActiveRecord::Base
     end
   end
 
-  def update_frequency_in_seconds(period, frequency_in_periods)
-    period_in_seconds = { 'hourly' => 3600, 'daily' => 86400, 'weekly' => 604800,
+  def update_period_in_seconds(period_units, period_in_units)
+    unit_in_seconds = { 'hourly' => 3600, 'daily' => 86400, 'weekly' => 604800,
       'monthly' => 2419200, 'yearly' => 31449600}
-    period_in_seconds[period] * frequency_in_periods
+    unit_in_seconds[period_units] * period_in_units
   end
 
 end
