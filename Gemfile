571afa1e0e045f7149e99d8252c41ff224453e79
Zack M. Davis <code@zackmdavis.net>
2013-11-06 19:26:13 -0800

    configuration tweaks for Heroku

--- Gemfile
+++ Gemfile
@@ -23,3 +23,5 @@ group :assets do
   gem 'coffee-rails', '~> 3.2.1'
   gem 'uglifier', '>= 1.0.3'
 end
+
+ruby "1.9.3"
--- config/application.rb
+++ config/application.rb
@@ -62,6 +62,9 @@ module Superscription
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # thanks to http://stackoverflow.com/a/19650687
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
