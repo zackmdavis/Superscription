b348971f34b1b1e66e8b70c69e020b34c75f8dac
Zack M. Davis <code@zackmdavis.net>
2013-11-12 11:17:05 -0800

    linking to bootstrap tabs
    With many thanks to http://stackoverflow.com/a/10787789

--- app/assets/javascripts/read.js
+++ app/assets/javascripts/read.js
@@ -1,11 +1,20 @@
 $(document).ready(function(){
 
-  $(".mark-as-read").click(function(event) {
+  $('#subscription-tabs a:first').tab('show');
+
+  var active_tab = window.location.hash;
+  var prefix = "_"
+  $('#subscription-tabs a[href="' + active_tab.replace(prefix, '') + '"]').tab('show');
+
+  $('#subscription-tabs a').on('click', function(event) {
+    window.location.hash = event.target.hash.replace("#", "#" + prefix);
+  });
+
+  $('.mark-as-read').click(function(event) {
     event.preventDefault();
     var entry_id = $(this).data("id");
     var well_id = "#entry-well-" + entry_id;
     $(well_id).fadeTo(250, 0.3);
-    // why isn't this working??
     $.ajax({
       url: "/api/readings",
       type: "POST",
